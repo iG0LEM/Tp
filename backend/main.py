f6b2c94fbee5796e59890d72291d442c8c00623e
Ivan Bubanj <ibubanj@fi.uba.ar>
2024-07-05 16:08:45 -0300

    cree un main para poder correr la app

---
+++ backend/main.py
@@ -0,0 +1,85 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from modelo import db, Equipo, Trofeo, Ganador, Finalista, Goleador
+
+app = Flask(__name__)
+port=5000
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+@app.route('/equipos', methods=['GET'])
+def get_equipos():
+    equipos = Equipo.query.all()
+    return jsonify([equipo.pais for equipo in equipos])
+
+@app.route('/equipos', methods=['POST'])
+def post_equipo():
+    pais = request.json.get('pais')
+    equipo = Equipo(pais=pais)
+    db.session.add(equipo)
+    db.session.commit()
+    return jsonify(equipo.pais)
+
+@app.route('/trofeos', methods=['GET'])
+def get_trofeos():
+    trofeos = Trofeo.query.all()
+    return jsonify([trofeo.nombre for trofeo in trofeos])
+
+@app.route('/trofeos', methods=['POST'])
+def post_trofeo():
+    nombre = request.json.get('nombre')
+    equipo_id = request.json.get('equipo_id')
+    trofeo = Trofeo(nombre=nombre, equipo_id=equipo_id)
+    db.session.add(trofeo)
+    db.session.commit()
+    return jsonify(trofeo.nombre)
+
+@app.route('/ganadores', methods=['GET'])
+def get_ganadores():
+    ganadores = Ganador.query.all()
+    return jsonify([ganador.nombre for ganador in ganadores])
+
+@app.route('/ganadores', methods=['POST'])
+def post_ganador():
+    nombre = request.json.get('nombre')
+    equipo_id = request.json.get('equipo_id')
+    ganador = Ganador(nombre=nombre, equipo_id=equipo_id)
+    db.session.add(ganador)
+    db.session.commit()
+    return jsonify(ganador.nombre)
+
+@app.route('/finalistas', methods=['GET'])
+def get_finalistas():
+    finalistas = Finalista.query.all()
+    return jsonify([finalista.id for finalista in finalistas])
+
+@app.route('/finalistas', methods=['POST'])
+def post_finalista():
+    equipo_id = request.json.get('equipo_id')
+    trofeo_id = request.json.get('trofeo_id')
+    finalista = Finalista(equipo_id=equipo_id, trofeo_id=trofeo_id)
+    db.session.add(finalista)
+    db.session.commit()
+    return jsonify(finalista.id)
+
+@app.route('/goleadores', methods=['GET'])
+def get_goleadores():
+    goleadores = Goleador.query.all()
+    return jsonify([goleador.nombre for goleador in goleadores])
+
+@app.route('/goleadores', methods=['POST'])
+def post_goleador():
+    nombre = request.json.get('nombre')
+    goles = request.json.get('goles')
+    equipo_id = request.json.get('equipo_id')
+    goleador = Goleador(nombre=nombre, goles=goles, equipo_id=equipo_id)
+    db.session.add(goleador)
+    db.session.commit()
+    return jsonify(goleador.nombre)
+
+if __name__ == '__main__':
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.run(port=port, debug=True)
+
